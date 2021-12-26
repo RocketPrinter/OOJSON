4f9622fd6ea7a371537998b2dc89201181b75009
RocketPrinter <35203190+RocketPrinter@users.noreply.github.com>
2021-12-26 21:52:41 +0200

    refactoring

--- OOJSON/OOJSolver.cs
+++ OOJSON/OOJSolver.cs
@@ -6,14 +6,21 @@ public record OOJSolverOptions(string nameProperty="ooj_name", string inheritPro
 
 public class OOJSolver
 {
+    record Document(string? name, List<string>? dependencies, JsonNode root);
+
     OOJSolverOptions options;
-    List<(string? name, List<string>? inheritance, JsonNode root)> docs = new();
+    List<Document> docs = new();
 
     public OOJSolver(OOJSolverOptions? options=null)
     {
         this.options = options ?? new();
     }
 
+    /// <summary>
+    /// Add a JSONObject that is the root of a document to the solver.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <exception cref="ArgumentException"></exception>
     public void AddRootJSONObject(JsonObject obj)
     {
         if (obj.Root != null) throw new ArgumentException("JsonObject is not root");
@@ -30,11 +37,49 @@ public class OOJSolver
            if (options.throwOnInvalidOOJ) throw;
         }
 
-        docs.Add((name, inheritance, obj));
+        docs.Add(new(name, inheritance, obj));
     }
 
-    public List<JsonNode> Solve()
+    public IEnumerable<JsonNode> Solve()
     {
+        // solve strings in dependencies
 
+        // topological sorting
+        Stack<Document> docStack = new();
+        HashSet<Document> visited = new();
+        foreach (var doc in docs)
+                TopoSort(doc);
+
+        void TopoSort(Document doc)
+        {
+            if (!visited.Contains(doc)) return;
+            visited.Add(doc);
+
+            foreach (string name in doc.dependencies ?? Enumerable.Empty<string>())
+            {
+                var child = docs.Find(x => x.name == name);
+                if (child != null)
+                    TopoSort(child);    
+                
+                if (options.throwOnInvalidOOJ)
+                    throw new Exception($"Cannot find OOJ with name {name}");
+            }
+
+            docStack.Push(doc);
+        }
+
+        // solve
+        List<JsonNode> result = new();
+        foreach (var doc in docStack)
+        {
+            result.Add(doc.root);
+
+        }
+
+        void Apply(JsonNode node, JsonNode baseNode)
+        {
+
+        }
+        return result;
     }
 }
